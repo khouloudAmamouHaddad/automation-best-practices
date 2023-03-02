58a327a51a0c6ccd76e6cf813e1f96b87eb04b57
khouloudAmamouHaddad <khouloud.amamouhaddad@gmail.com>
2023-03-02 13:35:38 +0100

    Solution to Exercice

--- automated-atomic-tests/cypress/integration/exercise.spec.js
+++ automated-atomic-tests/cypress/integration/exercise.spec.js
@@ -17,18 +17,20 @@ describe('Exercise for automated atomic tests', () => {
       });
     });
   
-    it('should be visible', () => {
+    it('Login Page should be visible', () => {
       /** Your code below */
       //Use the LoginPage page to check that the `screen.should('be.visible')`
       /** Your code above */
+      LoginPage.screen.should('be.visible');
     });
   
-    it('allows UI login with a standard user', () => {
+    it('Allows UI login with a standard user', () => {
       /** Your code below */
 
       //1. Use the LoginPage page to signIn(LOGIN_USERS.STANDARD)
+      LoginPage.signIn(LOGIN_USERS.STANDARD)
       //2. Now check that the ProductsPage.screen is visible. 💡 the same as the test above, only with a different page object
-      
+      ProductsPage.screen.should("be.visible");
       /** Your code above */
     });
   });
@@ -40,9 +42,14 @@ describe('Exercise for automated atomic tests', () => {
       /** Your code below */
 
       //1. Use the setTestContext() to set the sessionStorage first
+      cy.setTestContext({
+        user: LOGIN_USERS.STANDARD,
+        path: PAGES.INVENTORY
+      });
       //2. Use the ProductsPage to addItemToCart(0);
+      ProductsPage.addItemToCart(0)
       //3. Now AppHeader.cart should('have.text', '1');
-      
+      AppHeader.cart.should('have.text',1)
       /** Your code above */
     });
   });
